1926a7ad20f4080196e6dfd0477b15a073d9c6ea
Aakash885 <154004648+Aakash885@users.noreply.github.com>
2024-02-04 23:06:14 +0530

    RewardValue Class added

--- src/main/java/RewardsConverter.java
+++ src/main/java/RewardsConverter.java
@@ -2,37 +2,7 @@ import java.util.Scanner;
 
 public class RewardsConverter {
     public static void main(String[] args) {
-        class RewardValue {
-            private double cashValue;
-            private double milesValue;
-
-            public RewardValue(double cashValue) {
-                this.cashValue = cashValue;
-                this.milesValue = cashValue / 0.0035;
-            }
-
-            public RewardValue(double milesValue, boolean isMiles) {
-                if (isMiles) {
-                    this.milesValue = milesValue;
-                    // Convert miles to cash at a rate of 0.0035
-                    this.cashValue = milesValue * 0.0035;
-                } else {
-                    this.cashValue = milesValue;
-                    this.milesValue = milesValue / 0.0035;
-                }
-            }
-
-            // Getter method for cash value
-            public double getCashValue() {
-                return cashValue;
-            }
-
-            // Getter method for miles value
-            public double getMilesValue() {
-                return milesValue;
-            }
-        }
-
+        
         var scanner = new Scanner(System.in);
         System.out.println("Welcome to the Credit Card Rewards Converter!");
         System.out.println("Please enter a cash value to convert to airline miles: ");
