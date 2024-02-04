e263c79c7a3c97bb4d8f98b69b574190ebcd9aa4
Aakash885 <154004648+Aakash885@users.noreply.github.com>
2024-02-04 23:46:47 +0530

    test resolve

--- src/main/java/RewardValue.java
+++ src/main/java/RewardValue.java
@@ -1,32 +1,32 @@
 
 public class RewardValue {
-        private double cashValue;
-        private double milesValue;
+    private double cashValue;
+    private double milesValue;
 
-        public RewardValue(double cashValue) {
-            this.cashValue = cashValue;
-            this.milesValue = cashValue / 0.0035;
-        }
+    public RewardValue(double cashValue) {
+        this.cashValue = cashValue;
+        this.milesValue = cashValue / 0.0035;
+    }
 
-        public RewardValue(double milesValue, boolean isMiles) {
-            if (isMiles) {
-                this.milesValue = milesValue;
-                // Convert miles to cash at a rate of 0.0035
-                this.cashValue = milesValue * 0.0035;
-            } else {
-                this.cashValue = milesValue;
-                this.milesValue = milesValue / 0.0035;
-            }
+    public RewardValue(double milesValue, boolean isMiles) {
+        if (isMiles) {
+            this.milesValue = milesValue;
+            // Convert miles to cash at a rate of 0.0035
+            this.cashValue = milesValue * 0.0035;
+        } else {
+            this.cashValue = milesValue;
+            this.milesValue = milesValue / 0.0035;
         }
+    }
 
-        // Getter method for cash value
-        public double getCashValue() {
-            return cashValue;
-        }
+    // Getter method for cash value
+    public double getCashValue() {
+        return cashValue;
+    }
 
-        // Getter method for miles value
-        public double getMilesValue() {
-            return milesValue;
-        }
+    // Getter method for miles value
+    public double getMilesValue() {
+        return milesValue;
+    }
 
 }
